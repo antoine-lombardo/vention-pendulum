e28c1e4b0868886aba767621f210fb03820d33a3
Antoine Lombardo <lombardoa@oxoc.net>
2025-09-28 15:36:40 -0400

    Added ESLint and Prettier

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,30 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  {
+    rules: {
+      '@typescript-eslint/no-namespace': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+
+      
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+          singleQuote: true,
+        },
+      ],
+    },
+  },
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
+);
