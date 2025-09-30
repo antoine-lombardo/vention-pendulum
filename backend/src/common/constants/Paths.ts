40a9312334939d222a415e8efaf5e9b1d010eb11
Antoine Lombardo <lombardoa@oxoc.net>
2025-09-30 19:47:51 -0400

    Removed unused files created on project generation

--- backend/src/common/constants/Paths.ts
+++ backend/src/common/constants/Paths.ts
@@ -1,12 +1,5 @@
 export default {
   Base: '/api',
-  Users: {
-    Base: '/users',
-    Get: '/all',
-    Add: '/add',
-    Update: '/update',
-    Delete: '/delete/:id',
-  },
   Simulation: {
     Base: '/simulation',
     Start: '/start',
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,27 +2,12 @@ import { Router } from 'express';
 
 import Paths from '@src/common/constants/Paths';
 import SimulationRoutes from './SimulationRoutes';
-import UserRoutes from './UserRoutes';
 
 /******************************************************************************
                                 Setup
 ******************************************************************************/
 
 const apiRouter = Router();
-
-// ** Add UserRouter ** //
-
-// Init router
-const userRouter = Router();
-
-// Get all users
-userRouter.get(Paths.Users.Get, UserRoutes.getAll);
-userRouter.post(Paths.Users.Add, UserRoutes.add);
-userRouter.put(Paths.Users.Update, UserRoutes.update);
-userRouter.delete(Paths.Users.Delete, UserRoutes.delete);
-
-// Add UserRouter
-apiRouter.use(Paths.Users.Base, userRouter);
 
 // ** Add SimulationRouter ** //
 
